27a18143159de7937e64a600bcd68803497d1838
kkniffin <kyle@kniffins.com>
2016-09-27 15:38:32 -0400

    Update start-elastalert.sh

--- start-elastalert.sh
+++ start-elastalert.sh
@@ -15,19 +15,20 @@ fi
 # In addition you may want to add the SYS_NICE capability, in order for ntpd to be able to modify its priority.
 ntpd -s
 
+
 # Wait until Elasticsearch is online since otherwise Elastalert will fail.
 rm -f garbage_file
-while ! wget -O garbage_file ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT} 2>/dev/null
+while ! wget --user=${ELASTICSEARCH_USER} --password=${ELASTICSEARCH_PASSWORD} -O garbage_file ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT} 2>/dev/null
 do
 	echo "Waiting for Elasticsearch..."
 	rm -f garbage_file
 	sleep 1
 done
 rm -f garbage_file
-sleep 5
+sleep 10
 
 # Check if the Elastalert index exists in Elasticsearch and create it if it does not.
-if ! wget -O garbage_file ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status 2>/dev/null
+if ! wget --user=${ELASTICSEARCH_USER} --password=${ELASTICSEARCH_PASSWORD} -O garbage_file  ${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}/elastalert_status 2>/dev/null
 then
 	echo "Creating Elastalert index in Elasticsearch..."
     elastalert-create-index --index elastalert_status --old-index ""
